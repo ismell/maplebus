634feb61ae4f88ec81336d7d3ebcaf55287f00f0
Raul E Rangel <me@aida-ipod.ad.ismell.org>
2014-10-13 12:58:23 -0600

    Enabled debug for fsbl

--- components/bootloader/zynq_fsbl/fsbl_debug.h
+++ components/bootloader/zynq_fsbl/fsbl_debug.h
@@ -76,6 +76,8 @@ extern "C" {
 #define DEBUG_GENERAL	0x00000001    /* general debug  messages */
 #define DEBUG_INFO	0x00000002    /* More debug information */
 
+#define FSBL_DEBUG_INFO 1
+
 #if defined (FSBL_DEBUG_INFO)
 #define fsbl_dbg_current_types ((DEBUG_INFO) | (DEBUG_GENERAL))
 #elif defined (FSBL_DEBUG)
