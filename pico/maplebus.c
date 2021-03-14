8c75c7240e1a40dc4384a388a4b1dddbbd856b45
Raul E Rangel <ismell@ismell.org>
2021-03-14 13:58:07 +0000

    Fix whitespace
    Signed-off-by: Raul E Rangel <ismell@ismell.org>

--- pico/maplebus.c
+++ pico/maplebus.c
@@ -22,50 +22,50 @@ uint8_t compute_lrc(struct maplebus_buffer *buffer) {
 	uint32_t *data = (uint32_t *)buffer;
 
 	for (size_t i = 0; i < buffer->header.length + 1; ++i) {
-	  lrc ^= (data[i] >> 0) & 0xFF;
-	  lrc ^= (data[i] >> 8) & 0xFF;
-	  lrc ^= (data[i] >> 16) & 0xFF;
-	  lrc ^= (data[i] >> 24) & 0xFF;
+		lrc ^= (data[i] >> 0) & 0xFF;
+		lrc ^= (data[i] >> 8) & 0xFF;
+		lrc ^= (data[i] >> 16) & 0xFF;
+		lrc ^= (data[i] >> 24) & 0xFF;
 	}
 
 	return lrc;
 }
 
 void maplebus_tx_program_init(PIO pio, uint sm, uint offset, uint pin_sdcka, uint pin_sdckb) {
-    pio_sm_config c = maplebus_tx_program_get_default_config(offset);
+	pio_sm_config c = maplebus_tx_program_get_default_config(offset);
 
-    // IO mapping
-    sm_config_set_set_pins(&c, pin_sdcka, 1);
-    sm_config_set_sideset_pins(&c, pin_sdckb);
-    sm_config_set_out_pins(&c, pin_sdckb, 1);
+	// IO mapping
+	sm_config_set_set_pins(&c, pin_sdcka, 1);
+	sm_config_set_sideset_pins(&c, pin_sdckb);
+	sm_config_set_out_pins(&c, pin_sdckb, 1);
 
-    // We need to clock out the MSB first, so we need to shift
-    // to the left.
-    sm_config_set_out_shift(&c, false, true, 0);
+	// We need to clock out the MSB first, so we need to shift
+	// to the left.
+	sm_config_set_out_shift(&c, false, true, 0);
 
-    // TODO: Remove the 1000 when we have a large pull-up.
-    float div = (float)clock_get_hz(clk_sys) / (25000000) * 1000; 
-    sm_config_set_clkdiv(&c, div);
+	// TODO: Remove the 1000 when we have a large pull-up.
+	float div = (float)clock_get_hz(clk_sys) / (25000000) * 1000; 
+	sm_config_set_clkdiv(&c, div);
 
-    uint32_t both_pins = (1u << pin_sdcka) | (1u << pin_sdckb);
-    pio_sm_set_pins_with_mask(pio, sm, 0, both_pins);
-    pio_sm_set_pindirs_with_mask(pio, sm, both_pins, both_pins);
-    pio_gpio_init(pio, pin_sdcka);
-    gpio_set_oeover(pin_sdcka, GPIO_OVERRIDE_INVERT);
-    pio_gpio_init(pio, pin_sdckb);
-    gpio_set_oeover(pin_sdckb, GPIO_OVERRIDE_INVERT);
+	uint32_t both_pins = (1u << pin_sdcka) | (1u << pin_sdckb);
+	pio_sm_set_pins_with_mask(pio, sm, 0, both_pins);
+	pio_sm_set_pindirs_with_mask(pio, sm, both_pins, both_pins);
+	pio_gpio_init(pio, pin_sdcka);
+	gpio_set_oeover(pin_sdcka, GPIO_OVERRIDE_INVERT);
+	pio_gpio_init(pio, pin_sdckb);
+	gpio_set_oeover(pin_sdckb, GPIO_OVERRIDE_INVERT);
 
-    gpio_pull_up(pin_sdcka);
-    gpio_pull_up(pin_sdckb);
+	gpio_pull_up(pin_sdcka);
+	gpio_pull_up(pin_sdckb);
 
-    // Clear IRQ flag before starting
-    hw_clear_bits(&pio->inte0, 1u << sm);
-    hw_clear_bits(&pio->inte1, 1u << sm);
-    pio->irq = 1u << sm;
+	// Clear IRQ flag before starting
+	hw_clear_bits(&pio->inte0, 1u << sm);
+	hw_clear_bits(&pio->inte1, 1u << sm);
+	pio->irq = 1u << sm;
 
-    // Configure and start SM
-    pio_sm_init(pio, sm, offset + maplebus_tx_offset_entry_point, &c);
-    pio_sm_set_enabled(pio, sm, true);
+	// Configure and start SM
+	pio_sm_init(pio, sm, offset + maplebus_tx_offset_entry_point, &c);
+	pio_sm_set_enabled(pio, sm, true);
 }
 
 int pio_maplebus_tx_blocking(PIO pio, uint sm, struct maplebus_header *data) {
@@ -88,34 +88,34 @@ int pio_maplebus_tx_blocking(PIO pio, uint sm, struct maplebus_header *data) {
 }
 
 void maplebus_rx_program_init(PIO pio, uint sm, uint offset, uint pin_sdcka, uint pin_sdckb) {
-    assert(pin_sdcka == pin_sdckb + 1);
-    pio_sm_set_consecutive_pindirs(pio, sm, pin_sdckb, 2, false);
-    pio_sm_config c = maplebus_rx_program_get_default_config(offset);
+	assert(pin_sdcka == pin_sdckb + 1);
+	pio_sm_set_consecutive_pindirs(pio, sm, pin_sdckb, 2, false);
+	pio_sm_config c = maplebus_rx_program_get_default_config(offset);
 
-    // IO mapping
-    sm_config_set_in_pins(&c, pin_sdckb);
-    sm_config_set_jmp_pin(&c, pin_sdcka);
+	// IO mapping
+	sm_config_set_in_pins(&c, pin_sdckb);
+	sm_config_set_jmp_pin(&c, pin_sdcka);
 
-    // We use the OSR as a temp register
-    sm_config_set_out_shift(&c, true, false, 0);
-    sm_config_set_in_shift(&c, false, false, 0);
+	// We use the OSR as a temp register
+	sm_config_set_out_shift(&c, true, false, 0);
+	sm_config_set_in_shift(&c, false, false, 0);
 
-    // We need to run fast so we can leave enough wiggle room for the
-    // CPU to decide how much data it wants to read.
-    float div = 1.0f;
-    sm_config_set_clkdiv(&c, div);
+	// We need to run fast so we can leave enough wiggle room for the
+	// CPU to decide how much data it wants to read.
+	float div = 1.0f;
+	sm_config_set_clkdiv(&c, div);
 
-    gpio_pull_up(pin_sdcka);
-    gpio_pull_up(pin_sdckb);
+	gpio_pull_up(pin_sdcka);
+	gpio_pull_up(pin_sdckb);
 
-    // Clear IRQ flag before starting
-    hw_clear_bits(&pio->inte0, 1u << sm);
-    hw_clear_bits(&pio->inte1, 1u << sm);
-    pio->irq = 1u << sm;
+	// Clear IRQ flag before starting
+	hw_clear_bits(&pio->inte0, 1u << sm);
+	hw_clear_bits(&pio->inte1, 1u << sm);
+	pio->irq = 1u << sm;
 
-    // Configure and start SM
-    pio_sm_init(pio, sm, offset + maplebus_rx_offset_entry_point, &c);
-    pio_sm_set_enabled(pio, sm, true);
+	// Configure and start SM
+	pio_sm_init(pio, sm, offset + maplebus_rx_offset_entry_point, &c);
+	pio_sm_set_enabled(pio, sm, true);
 }
 
 enum maplebus_return pio_maplebus_rx_blocking(PIO pio, uint sm, struct maplebus_header *data, size_t n) {
